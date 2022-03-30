e6c8d3dcf95364f854b0159f0bf02f7d99715e0f
Victor Carreno <vmanuelcar@hotmail.com>
2022-03-29 21:17:45 -0500

    New mode and fixes

--- index.js
+++ index.js
@@ -55,6 +55,19 @@ function cargarNube2(){
     nube_2.cargado = true;
 }
 
+let nube_3 = {
+    cargado: false,
+    url: "assets/cloud_3.png"
+}
+
+nube_3.objeto = new Image();
+nube_3.objeto.src = nube_3.url;
+nube_3.objeto.addEventListener("load", cargarNube3);
+
+function cargarNube3(){
+    nube_3.cargado = true;
+}
+
 let titulo = {
     cargado: false,
     url: "assets/main_title.png"
@@ -118,6 +131,45 @@ zero.objeto.addEventListener("load", cargar0);
 
 function cargar0(){
     zero.cargado = true;
+}
+
+let morio_face = {
+    cargado: false,
+    url: "assets/morio_face.png"
+}
+
+morio_face.objeto = new Image();
+morio_face.objeto.src = morio_face.url;
+morio_face.objeto.addEventListener("load", cargarMorio_face);
+
+function cargarMorio_face(){
+    morio_face.cargado = true;
+}
+
+let morio_stand = {
+    cargado: false,
+    url: "assets/morio_stand.png"
+}
+
+morio_stand.objeto = new Image();
+morio_stand.objeto.src = morio_stand.url;
+morio_stand.objeto.addEventListener("load", cargarMorio_stand);
+
+function cargarMorio_stand(){
+    morio_stand.cargado = true;
+}
+
+let morio_frame2 = {
+    cargado: false,
+    url: "assets/morio_frame2.png"
+}
+
+morio_frame2.objeto = new Image();
+morio_frame2.objeto.src = morio_frame2.url;
+morio_frame2.objeto.addEventListener("load", cargarMorio_frame2);
+
+function cargarMorio_frame2(){
+    morio_frame2.cargado = true;
     executeStart()
 }
 
@@ -129,23 +181,13 @@ function esperar(miliseconds){
 
 function executeStart(){
     const process = async () => {
-        let nube_1_x = 400;
-        const nube_1_y = -30;
-        let nube_2_x = 800
-        const nube_2_y = 180;
-        const titulo_x = 300;
-        const titulo_y = 150;
-        const suelo_x = 0;
-        const suelo_y = 750;
-        const press_start_x = 480;
-        const press_start_y = 500;
-        const coin_x = 490;
-        const coin_y = 45;
-        const coin_signal_x = 570;
-        const coin_signal_y = 70;
-        const zero_x = 600;
-        const zero_y = 63;
         // Draw image
+        let nube_1_x = 600;
+        const nube_1_y = -30;
+        let nube_2_x = 800;
+        const nube_2_y = 100;
+        let nube_3_x = 80;
+        const nube_3_y = 190;
         for (i = 0; i < 60; i++){
             await esperar(30)
             if (fondo_background.cargado){
@@ -157,42 +199,151 @@ function executeStart(){
             if (nube_2.cargado){
                 canvas_2d.drawImage(nube_2.objeto, nube_2_x, nube_2_y, 200, 160);
             }
+            if (nube_3.cargado){
+                canvas_2d.drawImage(nube_3.objeto, nube_3_x, nube_3_y, 280, 140);
+            }
             if (titulo.cargado){
-                canvas_2d.drawImage(titulo.objeto, titulo_x, titulo_y, 566, 307);
+                canvas_2d.drawImage(titulo.objeto, 300, 150, 566, 307);
             }
             if (suelo.cargado){
-                canvas_2d.drawImage(suelo.objeto, suelo_x, suelo_y, 1200, 50);
+                canvas_2d.drawImage(suelo.objeto, 0, 750, 1200, 50);
             }
             if (coin.cargado){
-                canvas_2d.drawImage(coin.objeto, coin_x, coin_y, 70, 70);
+                canvas_2d.drawImage(coin.objeto, 510, 45, 70, 70);
             }
             if (x_signal.cargado){
-                canvas_2d.drawImage(x_signal.objeto, coin_signal_x, coin_signal_y, 15, 15);
+                /* Cross Coin */
+                canvas_2d.drawImage(x_signal.objeto, 590, 70, 15, 15);
+                /* Cross Morio */
+                canvas_2d.drawImage(x_signal.objeto, 180, 73, 15, 15);
             }
             if (zero.cargado){
-                canvas_2d.drawImage(zero.objeto, zero_x, zero_y, 30, 30);
+                canvas_2d.drawImage(zero.objeto, 620, 63, 30, 30);
+                canvas_2d.drawImage(zero.objeto, 210, 65, 30, 30);
+            }
+            if (morio_face.cargado){
+                canvas_2d.drawImage(morio_face.objeto, 100, 50, 65, 60);
+            }
+            if (morio_stand.cargado){
+                canvas_2d.drawImage(morio_stand.objeto, 300, 610, 85, 140);
             }
     
             // Movement
             if (i === 59){i = 0}
-            nube_1_x = nube_1_x - 1;
+            nube_1_x = nube_1_x - 0.5;
             nube_2_x = nube_2_x - 2;
+            nube_3_x = nube_3_x - 1;
             if (nube_1_x === -450){
                 nube_1_x = 1200
             }
             if (nube_2_x === -290){
                 nube_2_x = 1200
             }
+            if (nube_3_x === -290){
+                nube_3_x = 1200
+            }
             if (i < 30){
-                canvas_2d.drawImage(press_start.objeto, press_start_x, press_start_y, 230, 27);
+                canvas_2d.drawImage(press_start.objeto, 480, 500, 230, 27);
+            }
+            document.addEventListener("keydown", endStart);
+            function endStart(event){
+                if (event.keyCode === 13){
+                    i = 60;
+                }
             }
         }
     }
     process();
-    document.addEventListener("keyup", firstStart);
-    function firstStart(event){
+}
+
+document.addEventListener("keydown", startMainGame);
+function startMainGame(event){
     if (event.keyCode === 13){
-        console.log("SOS")
+        mainGame();
+    }
+}
+
+const keys = {
+    LEFT: 37,
+    RIGHT: 39
+  };
+
+function mainGame(){
+    const gameProcess = async () => {
+        await esperar(30)
+        let nube_1_x = 300;
+        const nube_1_y = -30;
+        let nube_2_x = 900;
+        const nube_2_y = 100;
+        let nube_3_x = 170;
+        const nube_3_y = 190;
+        for (i = 0; i < 60; i++){
+            await esperar(30)
+            if (fondo_background.cargado){
+                canvas_2d.drawImage(fondo_background.objeto, 0, 0, 1200, 800);
+            }
+            if (nube_1.cargado){
+                canvas_2d.drawImage(nube_1.objeto, nube_1_x, nube_1_y, 500, 370);
+            }
+            if (nube_2.cargado){
+                canvas_2d.drawImage(nube_2.objeto, nube_2_x, nube_2_y, 200, 160);
+            }
+            if (nube_3.cargado){
+                canvas_2d.drawImage(nube_3.objeto, nube_3_x, nube_3_y, 280, 140);
+            }
+            if (suelo.cargado){
+                canvas_2d.drawImage(suelo.objeto, 0, 750, 1200, 50);
+            }
+            if (coin.cargado){
+                canvas_2d.drawImage(coin.objeto, 510, 45, 70, 70);
+            }
+            if (x_signal.cargado){
+                /* Cross Coin */
+                canvas_2d.drawImage(x_signal.objeto, 590, 70, 15, 15);
+                /* Cross Morio */
+                canvas_2d.drawImage(x_signal.objeto, 180, 73, 15, 15);
+            }
+            if (zero.cargado){
+                canvas_2d.drawImage(zero.objeto, 620, 63, 30, 30);
+                canvas_2d.drawImage(zero.objeto, 210, 65, 30, 30);
+            }
+            if (morio_face.cargado){
+                canvas_2d.drawImage(morio_face.objeto, 100, 50, 65, 60);
+            }
+            // General Movement
+            if (i === 59){i = 0}
+            let start = true;
+            nube_1_x = nube_1_x - 0.5;
+            nube_2_x = nube_2_x - 2;
+            nube_3_x = nube_3_x - 1;
+            if (nube_1_x === -450){
+                nube_1_x = 1200
+            }
+            if (nube_2_x === -290){
+                nube_2_x = 1200
+            }
+            if (nube_3_x === -290){
+                nube_3_x = 1200
+            }
+            // Morio Movement
+            let morio_x = 300;
+            if (morio_stand.cargado){
+                if (start){
+                    canvas_2d.drawImage(morio_stand.objeto, morio_x, 610, 85, 140);
+                }
+            }
+            document.addEventListener("keydown", moveMorio);
+            function moveMorio(event){
+                switch (event.keyCode){
+                    case keys.RIGHT:
+                        start = false;
+                        canvas_2d.drawImage(morio_frame2.objeto, morio_x, 610, 85, 140);
+                        morio_x = morio_x + 10;
+                        start = true;
+                    break;
+                }
+            }
         }
     }
+    gameProcess();
 }
